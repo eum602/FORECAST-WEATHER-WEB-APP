559bb733d40fa8b79e3427a9db540231bc09d1d0
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-06 12:05:48 -0500

    added partial templates

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const path = require('path') //core module
 const express =  require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+//defines paths for Exoress config
 const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath =  path.join(__dirname,'../templates/partials')
 
+//setup handle bars engine and  views location
+app.set('view engine','hbs') //setting the view engine as hbs, now we can create some dynamic content.
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath)) //to customize our server
 //static means items do not change, so if I refresh the page it will be showing the same.
 
-app.set('view engine','hbs') //setting the view engine as hbs, now we can create some dynamic content.
+
 
 app.get('',(req,res)=>{
     res.render('index',{
@@ -21,13 +31,31 @@ app.get('',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.render('about',{
         title:'About me',
-        name:'Erick'
+        name:'Erick Pacheco'
     })
 })
 
 app.get('/help',(req,res)=>{
     res.render('help',{
-        title: 'Help page'
+        title: 'Help',
+        name:'Erick Pacheco'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title: 'Article not found',
+        name:'Erick Pacheco',
+        errorMessage: 'Help article not found'
+    })
+})
+
+//lastly by default
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        name:'Erick Pacheco',
+        errorMessage: 'page not found'
     })
 })
 
