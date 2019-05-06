373fb1ea38970722125121ae34808aeda4f8be35
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-06 11:05:23 -0500

    added dynamic templates with hbs-removed static html files

--- src/app.js
+++ src/app.js
@@ -1,34 +1,40 @@
 const path = require('path') //core module
 const express =  require('express')
 
-//console.log(__dirname)
-//console.log(__filename)
-//console.log(path.join(__dirname,'../public')) //pointing to public folder
 const app = express()
+
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath)) //to customize our server
 //static means items do not change, so if I refresh the page it will be showing the same.
+
+app.set('view engine','hbs') //setting the view engine as hbs, now we can create some dynamic content.
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        name:'Erick Pacheco'
+    }) //instead send we use render in order to render the views, previously we must set the wiew engine
+    //index refers ti index.hbs
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name:'Erick'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title: 'Help page'
+    })
+})
+
 const port = 3000
-
-//it wont run if app.use(...) is running
-// app.get('',(req,res)=>{
-//     res.send('<h1>hello express</h1>')
-// })
-
-// app.get('/help',(req,res)=>{
-//     res.send({name:'Erick',age:27})
-// })
-
-// app.get('/about',(req,res)=>{
-//     res.send('<h1>about page</h1>')
-// })
 
 app.get('/weather',(req,res)=>{
     res.send({forecast:'It is snowing',location:'Punta Negra'})
 })
-
-//app.com
-//app.com/help
 
 app.listen(port,()=>{console.log(`Starting server on port ${port}`)})
